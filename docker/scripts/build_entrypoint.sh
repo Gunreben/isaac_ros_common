c03247d805bb3f964b8ea3133247f70ada47b930
Gunreben <gunreben@wifa.uni-leipzig.de>
2024-04-02 15:44:00 +0200

    added entrypoints for different use-cases

---
+++ docker/scripts/build_entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
+source /opt/ros/${ROS_DISTRO}/setup.bash
+
+echo "source /workspaces/isaac_ros-dev/install/setup.bash" >> ~/.bashrc
+source /workspaces/isaac_ros-dev/install/setup.bash
+
+# Restart udev daemon
+sudo service udev restart
+
+colcon build --symlink-install --cmake-clean-first 
+
+$@
---
+++ docker/scripts/publisher_launch_entrypoint.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
+source /opt/ros/${ROS_DISTRO}/setup.bash
+
+echo "source /workspaces/isaac_ros-dev/install/setup.bash" >> ~/.bashrc
+source /workspaces/isaac_ros-dev/install/setup.bash
+
+# Restart udev daemon
+sudo service udev restart
+
+ros2 launch arkvision_six_cameras combined_launch.py
+
+$@
