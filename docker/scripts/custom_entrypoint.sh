950d5bdd4c77a093f5588e083b322dfcbc839858
Gunreben <gunreben@wifa.uni-leipzig.de>
2024-04-02 14:38:49 +0200

    forgot shebang

--- docker/scripts/custom_entrypoint.sh
+++ docker/scripts/custom_entrypoint.sh
@@ -1,3 +1,5 @@
+#!/bin/bash
+
 echo "source /opt/ros/${ROS_DISTRO}/setup.bash" >> ~/.bashrc
 source /opt/ros/${ROS_DISTRO}/setup.bash
 
@@ -6,3 +8,7 @@ source /workspaces/isaac_ros-dev/install/setup.bash
 
 # Restart udev daemon
 sudo service udev restart
+
+ros2 launch arkvision_six_cameras combined_launch.py
+
+$@
